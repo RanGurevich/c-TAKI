b2562d72f66638ee5b2a33f41d38ad3e7f364ce5
Ran Gurevich <30481117+RanGurevich@users.noreply.github.com>
2023-01-21 02:56:26 +0200

    added change direction and change color cards, added remove cards and play check

--- taki/207685504-project.c
+++ taki/207685504-project.c
@@ -51,12 +51,15 @@ typedef struct UserStruct
 void *setUsers(int* numberOfPlayers);
 int getRandomNumber(int stratPoint, int endPoint);
 CARD getRandomCard();
+CARD changeColor();
 void setStartStackCard(CARD* startCard);
 void printCard(CARD cardToPrint);
-void printUserCard(USER user);
+void printUserCard(USER *user);
 void gameRun(USER* usersPlaying, int numberOfPlayers);
 void gameTurn(USER* userPlaying, int* direction);
 void takeCardFromDeck(USER* userplaying);
+void removeCardFromUserAndSetDeck(USER* user, int indexToRemove, CARD* cardOnStack);
+void putCardInGame(USER* userPlaying, int* direction, CARD* cardOnStack);
 
 void main() {
 	USER* usersInGame;
@@ -65,7 +68,7 @@ void main() {
 	srand(time(NULL));
 	setStartStackCard(&stackCard);
 	usersInGame = setUsers(&numberOfPlayers);
-	gameRun(usersInGame, numberOfPlayers, stackCard);
+	gameRun(usersInGame, numberOfPlayers, &stackCard);
 }
 
 int getRandomNumber(int startPoint, int endPoint) {
@@ -96,32 +99,39 @@ void *setUsers(int *numberOfPlayers) {
 	return usersInGame;
 }
 
-void gameRun(USER usersPlaying[], int numberOfPlayers, CARD cardOnStack) {
+void gameRun(USER usersPlaying[], int numberOfPlayers, CARD *cardOnStack) {
 	int playingNowIndex = 0;
 	bool someoneWon = false;
 	int direction = DIRECRTION_RIGHT;
 	while (!someoneWon)
 	{
+
 		printf("Upper card:\n");
-		printCard(cardOnStack);
-		gameTurn(&usersPlaying[playingNowIndex], &direction);
+		printCard(*cardOnStack);
+		gameTurn(&usersPlaying[playingNowIndex], &direction, cardOnStack);
 		if(usersPlaying[playingNowIndex].currentUserCards == 0) {
 			printf("AND THE WINNER IS...\n %s!!!!!!~~",usersPlaying[playingNowIndex].userName);
 			someoneWon = true;
 		}
 		playingNowIndex += direction;
-		if(playingNowIndex == numberOfPlayers || playingNowIndex == -1) {
+		if(playingNowIndex >= numberOfPlayers || playingNowIndex < 0) {
 			playingNowIndex = direction == DIRECRTION_RIGHT ? 0 : (numberOfPlayers - 1);
 		}
 
 	}
 }
-void gameTurn(USER *userPlaying, int* direction) {
+void gameTurn(USER *userPlaying, int* direction, CARD* cardOnStack) {
 	int userCardChoice;
 	printf("%s' turn!\n", userPlaying->userName);
-	printUserCard(*userPlaying);
-	printf("or 1 - %d if you want to put one of your cards in the middle : ", userPlaying)
+	printUserCard(userPlaying);
 	printf("Please enter 0 if you want to take a card from the deck\n");
+	printf("or 1 - %d if you want to put one of your cards in the middle : ", userPlaying->currentUserCards);
+	putCardInGame(userPlaying, direction, cardOnStack);
+
+}
+void putCardInGame(USER* userPlaying, int* direction, CARD* cardOnStack) {
+	int userCardChoice;
+	CARD systemSaveCard;
 	scanf("%d", &userCardChoice);
 	switch (userCardChoice)
 	{
@@ -129,6 +139,61 @@ void gameTurn(USER *userPlaying, int* direction) {
 		takeCardFromDeck(userPlaying);
 		break;
 	default:
+		//color
+		if (userPlaying->userCards[userCardChoice - 1].cardType == CHANGE_COLOR_CARD_INDEX) {
+			systemSaveCard = changeColor();
+			removeCardFromUserAndSetDeck(userPlaying, userCardChoice - 1, cardOnStack);
+			*cardOnStack = systemSaveCard;
+		} 
+		else
+		//normal card
+		if (userPlaying->userCards[userCardChoice - 1].cardColor == cardOnStack->cardColor || userPlaying->userCards[userCardChoice - 1].cardType == cardOnStack->cardType) {
+			systemSaveCard = userPlaying->userCards[userCardChoice - 1];
+			removeCardFromUserAndSetDeck(userPlaying, userCardChoice - 1, cardOnStack);
+			if (systemSaveCard.cardType == PLUS_CARD_INDEX) {
+				printf("+-+-+-+PLUS PLUS PLUS!!! %s USES THE PLUS CARD!!! PUT ANOTHER CARD!+-+-+-+", userPlaying->userName);
+				*cardOnStack = systemSaveCard;
+				printUserCard(userPlaying);
+				putCardInGame(userPlaying, direction, cardOnStack);
+			}
+			if (systemSaveCard.cardType == CHANGE_DIRECTION_CARD_INDEX) {
+				*direction *= -1;
+			}
+		}
+		else
+		{
+			printf("Invaid Card! please try again!\n");
+			printf("Please enter 0 if you want to take a card from the deck\n");
+			printf("or 1 - %d if you want to put one of your cards in the middle : ", userPlaying->currentUserCards);
+			putCardInGame(userPlaying, direction, cardOnStack);
+		}
+		break;
+	}
+}
+
+CARD changeColor() {
+	int userChoice;
+	CARD newColorCard;
+	newColorCard.cardType = CHANGE_COLOR_CARD_INDEX;
+	printf("Please enter your color choice:\n1 - Yellow\n2 - Red\n3 - Blue\n4 - Green\n");
+	scanf("%d", &userChoice);
+	switch (userChoice)
+	{
+	case 1:
+		newColorCard.cardColor = YELLOW_CHAR;
+		return newColorCard;
+	case 2:
+		newColorCard.cardColor = RED_CHAR;
+		return newColorCard;
+	case 3:
+		newColorCard.cardColor = BLUE_CHAR;
+		return newColorCard;
+	case 4:
+		newColorCard.cardColor = GREEN_CHAR;
+		return newColorCard;
+	default:
+		printf("Invalid color! Try again.\n");
+		return changeColor();
 		break;
 	}
 }
@@ -144,12 +209,12 @@ void takeCardFromDeck(USER* userplaying) {
 	userplaying->currentUserCards += 1;
 	userplaying->userCards[userplaying->currentUserCards - 1] = getRandomCard();
 }
-void printUserCard(USER user) {
+void printUserCard(USER *user) {
 	int i;
-	for ( i = 0; i < user.currentUserCards; i++)
+	for ( i = 0; i < user->currentUserCards; i++)
 	{
 		printf("Card #%d:\n",i+1);
-		printCard(user.userCards[i]);
+		printCard(user->userCards[i]);
 	}
 }
 void setStartStackCard(CARD *startCard) {
@@ -168,8 +233,8 @@ void printCard(CARD cardToPrint) {
 			printf("* STOP  *\n");
 		break;
 		case PLUS_CARD_INDEX:
-			printf("* PLUS  *\n");
-		break;
+			printf("*   +   *\n");
+			break;
 		case CHANGE_COLOR_CARD_INDEX:
 			printf("* COLOR *\n");
 		break;
@@ -188,6 +253,17 @@ void printCard(CARD cardToPrint) {
 	printf("*       *\n");
 	printf("*********\n");
 	printf("\n");
+}
+
+void removeCardFromUserAndSetDeck(USER* user, int indexToRemove, CARD* cardOnStack) {
+	int i;
+	cardOnStack->cardColor = user->userCards[indexToRemove].cardColor;
+	cardOnStack->cardType = user->userCards[indexToRemove].cardType;
+	for (i = indexToRemove; i < user->currentUserCards-1; i++)
+	{
+		user->userCards[i] = user->userCards[i + 1];
+	}
+	user->currentUserCards--;
 }
 CARD getRandomCard() {
 	CARD cardToSet;
@@ -214,7 +290,6 @@ CARD getRandomCard() {
 		case GREEN_INDEX:
 			cardToSet.cardColor = GREEN_CHAR;
 		break;
-		
 		default:
 			break;
 		}
